30f5cd0746424d6609d977d21d63ba66f471381f
Felipe Figueiredo <philsf79@gmail.com>
2021-09-18 19:11:14 -0300

    data input, reshape and labels

--- scripts/input.R
+++ scripts/input.R
@@ -7,9 +7,9 @@ library(labelled)
 
 # data loading ------------------------------------------------------------
 set.seed(42)
-data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
-# data.raw <- read_excel("dataset/file.xlsx") %>%
-#   janitor::clean_names()
+# data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
+data.raw <- read_excel("dataset/correlação escorpiões.xlsx") %>%
+  janitor::clean_names()
 
 
 # data cleaning -----------------------------------------------------------
@@ -20,17 +20,27 @@ data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
 
 # data wrangling ----------------------------------------------------------
 
-# data.raw <- data.raw %>%
-#   mutate(
-#
-#   )
+data.raw <- data.raw %>%
+  # Passo 1: pivotar para tabela longa
+  pivot_longer(
+    -ano,
+    names_to = c("tipo", "local"),
+    values_to = "eventos",
+    names_sep = "_",
+    ) %>%
+  # passo 2: pivotar de volta para ter as duas variáveis de interesse
+  pivot_wider(
+    names_from = tipo,
+    values_from = eventos,
+  )
 
 # labels ------------------------------------------------------------------
 
 data.raw <- data.raw %>%
   set_variable_labels(
-    group = "Study group",
-    outcome = "Study outcome",
+    ano = "Ano",
+    capturas = "Captura Noturna",
+    acidentes = "Acidentes escorpiônicos",
   )
 
 # analytical dataset ------------------------------------------------------
@@ -38,13 +48,14 @@ data.raw <- data.raw %>%
 analytical <- data.raw %>%
   # select analytic variables
   select(
-    id,
-    group,
-    outcome,
+    ano,
+    local,
+    capturas,
+    acidentes,
   )
 
 # mockup of analytical dataset for SAP and public SAR
-analytical_mockup <- tibble( id = c( "1", "2", "3", "...", as.character(nrow(analytical)) ) ) %>%
-  left_join(analytical %>% head(0), by = "id") %>%
+analytical_mockup <- tibble( ano = c( 1:3, "...", nrow(analytical) ) ) %>%
+  left_join(analytical %>% head(0) %>% mutate(ano=as.character(ano)), by = "ano") %>%
   mutate_all(as.character) %>%
   replace(is.na(.), "")

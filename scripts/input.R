202811175cf53cd2a30f70c3689ac9cd816ebb76
Felipe Figueiredo <philsf79@gmail.com>
2021-10-07 17:53:27 -0300

    new dataset included

--- scripts/input.R
+++ scripts/input.R
@@ -10,7 +10,8 @@ set.seed(42)
 # data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
 data.raw <- read_excel("dataset/APAsAno.xlsx") %>%
   janitor::clean_names()
-
+upa.raw <- read_excel("dataset/UPA.xlsx") %>%
+  janitor::clean_names()
 
 # data cleaning -----------------------------------------------------------
 
@@ -19,10 +20,29 @@ data.raw <- data.raw %>%
   # select()
   rename(
     pop = number_of_inhabitants,
+  )
+
+upa.raw <- upa.raw %>%
+  rename(
+    upa = urban_planning_areas_upa,
+    sewage = sewage_system_km,
+    rain = rainwater_network_km,
+    green = green_areas_m2,
+    garbage = irregular_garbage_disposal_areas,
+    junk = junkyards_units,
+    burn = burned_out_areas_foci,
+    recycling_inf = informal_recycling_units,
     area = urban_territorial_area_km2,
     hydrography = hydrography_m2,
     railway = railway_lines_meters,
-    recycling_no = municipal_recycling_centers_units,
+    recycling_mun = municipal_recycling_centers_units,
+  ) %>%
+  # identify UPAs with a cemitery
+  mutate(
+    cemitery = case_when(
+      upa %in% c(4, 6) ~ "1",
+      TRUE ~ "0"
+    )
   )
 
 # data wrangling ----------------------------------------------------------
@@ -43,10 +63,14 @@ data.raw <- data.raw %>%
     upa = "Urban Planning Area",
     accidents = "Number of accidents",
     pop = "Population",
+  )
+
+upa.raw <- upa.raw %>%
+  set_variable_labels(
     area = "Area (km²)",
     hydrography = "Hydrography area (m²)",
     railway = "Railway lines (m)",
-    recycling_no = "Number of recycling units",
+    recycling_mun = "Municipal recycling units",
   )
 
 # analytical dataset ------------------------------------------------------

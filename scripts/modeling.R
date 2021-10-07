15b2e55511c4150e59bd8fb31fdfcc24e43c7ebe
Felipe Figueiredo <philsf79@gmail.com>
2021-10-07 15:34:15 -0300

    tests: poisson models (Obs)
    - don't use the static vars (no variance)
    - drop mixed models approach

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -2,34 +2,66 @@
 # library(gt)
 library(gtsummary)
 # library(moderndive)
-# library(broom)
-library(lmerTest)
-library(broom.mixed)
+library(broom)
+# library(lmerTest)
+# library(broom.mixed)
 
 # raw estimate ------------------------------------------------------------
 
-modelo.glm <- glm(capturas ~ date_time:id, analytical, family = "poisson")
-modelo.glm %>%
+model.glm.min <- glm(
+  accidents ~ upa,
+  analytical, family = "poisson")
+model.glm.year <- glm(
+  accidents ~ upa + as.factor(year),
+  analytical, family = "poisson")
+model.glm.full <- glm(
+  accidents ~ upa + as.factor(year) + pop,
+  analytical, family = "poisson")
+
+model.glm.min %>%
+  summary()
+model.glm.year %>%
+  summary()
+model.glm.full %>%
   summary()
 
-modelo.glm %>%
+
+model.glm.min %>%
+  tidy()
+model.glm.year %>%
+  tidy()
+model.glm.full %>%
   tidy()
 
-modelo.glm %>%
+model.glm.min %>%
   glance()
+model.glm.year %>%
+  glance()
+model.glm.full %>%
+  glance()
+
+model.glm.min %>%
+  tbl_regression(exp = TRUE)
+
+model.glm.year %>%
+  tbl_regression(exp = TRUE)
+
+model.glm.full %>%
+  tbl_regression(exp = TRUE)
 
 # adjusted ----------------------------------------------------------------
 
-modelo.glmm.min <- glmer(capturas ~  (1 | quadra / sepultura), analytical, family = "poisson")
+# modelo.glmm.min <- 
+# glmer(accidents ~ upa  + as.factor(year) + (1 | upa), analytical, family = "poisson")
 
-modelo.glmm.min %>%
-  summary()
-
-modelo.glmm.min %>%
-  tidy()
-
-modelo.glmm.min %>%
-  glance()
-
-data.raw %>%
-  glmer(formula = capturas  ~ femeas_c_filhotes + temp_max_int + u_r_ext_percent + ( 1 | quadra), family = "poisson")
+# modelo.glmm.min %>%
+#   summary()
+# 
+# modelo.glmm.min %>%
+#   tidy()
+# 
+# modelo.glmm.min %>%
+#   glance()
+# 
+# data.raw %>%
+#   glmer(formula = capturas  ~ femeas_c_filhotes + temp_max_int + u_r_ext_percent + ( 1 | quadra), family = "poisson")

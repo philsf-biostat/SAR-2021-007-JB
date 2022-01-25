e791426a7eb1c82affe05806fff3ccb42bffbea2
Felipe Figueiredo <philsf79@gmail.com>
2022-01-25 18:11:49 -0300

    update: theme applied to all plots

--- scripts/plots.R
+++ scripts/plots.R
@@ -10,12 +10,15 @@ scale_fill_discrete <- function(...) scale_fill_brewer(palette = ff.pal, ...)
 
 gg <- analytical %>%
   ggplot() +
+  scale_color_brewer(palette = ff.pal) +
+  scale_fill_brewer(palette = ff.pal) +
   theme_ff()
 
-gg <- upa.raw %>%
+gg.upa <- upa.raw %>%
   select(-cemitery) %>%
   pivot_longer(-upa, names_to = "var", values_to = "val") %>%
   ggplot(aes(val)) +
+  theme_ff() +
   scale_color_brewer(palette = ff.pal) +
   scale_fill_brewer(palette = ff.pal) +
   xlab("") + ylab("") +
@@ -29,26 +32,28 @@ gg <- upa.raw %>%
 #   scale_fill_brewer(palette = ff.pal) +
 #   labs()
 
-gg.dens <- gg +
+gg.dens <- gg.upa +
   geom_density(fill = ff.col) +
   labs(title = "Distribution densities of APU characteristics")
 
 # cool facet trick from https://stackoverflow.com/questions/3695497 by JWilliman
-gg.hist <- gg +
+gg.hist <- gg.upa +
   geom_histogram(bins = 5, aes(y = ..count../tapply(..count.., ..PANEL.., sum)[..PANEL..]), fill = ff.col) +
   scale_y_continuous(labels = scales::label_percent(accuracy = 1)) +
   labs(title = "Distributions of APU characteristics")
 
-gg.pop <- analytical %>%
-  ggplot(aes(year, pop, group = upa, col = upa)) +
-  scale_color_brewer(palette = ff.pal) +
-  ylab("Population (per 10000)") +
-  xlab(NULL) +
-  geom_line(size = .7)
+gg.pop <- gg +
+  labs(
+    x = NULL,
+    y = "Population (per 10000)",
+    color = 'UPA',
+    ) +
+  geom_line(aes(year, pop, group = upa, col = upa), size = .7)
 
-gg.rate <- analytical %>%
-  ggplot(aes(year, accidents/pop, group = upa, col = upa)) +
-  scale_color_brewer(palette = ff.pal) +
-  ylab("Observed incidence rate (per 10000)") +
-  xlab(NULL) +
-  geom_line(size = .7, alpha = .8)
+gg.rate <- gg +
+  labs(
+    x = NULL,
+    y = "Observed incidence rate (per 10000)",
+    color = 'UPA',
+    ) +
+  geom_line(aes(year, accidents/pop, group = upa, col = upa), size = .7, alpha = .8)
